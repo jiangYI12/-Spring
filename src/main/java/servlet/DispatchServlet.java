06bc300c1e8a82505eae80568d09df578d898c22
jiangy@qrkeji.net <jiangy@qrkeji.net>
2019-08-09 18:02:05 +0800

    spring1.0 IOC容器简易

---
+++ src/main/java/servlet/DispatchServlet.java
@@ -0,0 +1,25 @@
+package servlet;
+
+import javax.servlet.ServletConfig;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.File;
+import java.io.IOException;
+import java.io.InputStream;
+import java.lang.reflect.Field;
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+import java.util.Properties;
+import java.util.concurrent.ConcurrentHashMap;
+
+/**
+ * Created by Tom on 2018/4/18.
+ */
+public class DispatchServlet extends HttpServlet {
+
+
+}
